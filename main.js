d0b0d694c34a880ae8ff8842cf674ef32d25d282
Johoan <38538750+Johoan@users.noreply.github.com>
2022-05-15 22:39:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $(".regbuttona").mouseover(function(){
+    $(".regbuttona").css("background-color", "white");
+    $(".regbuttona").css("color", "grey");
+    
+  });
+  $(".regbuttona").mouseout(function(){
+    $(".regbuttona").css("background-color", "#293f50");
+	$(".regbuttona").css("color", "#fff");
+	
+  });
+});
+
+$(document).ready(function(){
+  $(".jq").mouseover(function(){
+    
+    $(".jq").css("color", "#648880");
+    $(".jq").css("fontSize", "17px");
+    $("nav").css("background-color", "#f6f1d3");
+    $(".jq").css("textAlign", "center");
+  });
+  $(".jq").mouseout(function(){
+    
+	$(".jq").css("color", "#fff");
+	$(".jq").css("fontSize", "14px");
+	$(".jq").css("background-color", "#293f50");
+  });
+});
